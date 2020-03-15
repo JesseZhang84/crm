a1d6acadd03497a0f9b29e37571e273e110d15b3
zzx <625219786@qq.com>
2020-03-15 20:09:02 +0800

    添加swagger

--- crm-systemset/crm-systemset-service/src/main/java/com/crm/web/controller/RoleController.java
+++ crm-systemset/crm-systemset-service/src/main/java/com/crm/web/controller/RoleController.java
@@ -3,6 +3,8 @@ package com.crm.web.controller;
 import com.crm.bean.Role;
 import com.crm.exception.CrmException;
 import com.crm.service.RoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/role")
+@Api(value = "角色管理", tags = {"角色管理"})
 public class RoleController {
     @Autowired
     private RoleService roleService;
 
     @GetMapping("/selectById")
+    @ApiOperation("根据ID查询角色")
     public ResponseEntity selectById(Long id){
             Role role = roleService.findRoleById(id);
             return ResponseEntity.ok(role);
     }
+
 }
